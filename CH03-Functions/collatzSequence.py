a0d4f64a3b8b1f00073a1f0aafa7aaa6ea8eb9c3
Jerry <jerrynutt416@gmail.com>
2021-03-22 11:38:08 -0400

    input updated
    Add if name == main function and corrected input.

--- CH03-Functions/collatzSequence.py
+++ CH03-Functions/collatzSequence.py
@@ -1,8 +1,8 @@
 # The Collatz Sequence
-# Write a function named collatz with number parameter
+# Write a function named collatz with number parameter.
 # If number is even, return (number // 2) 
 # If number is odd, return (3 * number + 1)
-# Use try and except to make sure the user inputs a int type string
+# Use try and except for correct int input.
 
 def collatz(number):
     if number == 1:
@@ -10,17 +10,18 @@ def collatz(number):
     elif number % 2 == 0:
         number = (number//2)
     elif number % 2 == 1:
-        number =  (3 * number) + 1
+        number = (3 * number) + 1
     print(number)
     return collatz(number)
 
-while True:
-  number = input('Enter a number: ')
-  try:
-    collatz(int(number))
-    break
-  except:
-    print('Please enter an integer')
+if __name__ == '__main__':
+  while True:
+    number = input('Enter a integer: ')
+    try:
+      collatz(int(number))
+      break
+    except:
+      print('Please enter an integer')
 
 
 
