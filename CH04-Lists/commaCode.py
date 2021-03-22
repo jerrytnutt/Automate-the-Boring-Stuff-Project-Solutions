22e72b0fdcd1282590bb34679c4901751d109c77
Jerry <jerrynutt416@gmail.com>
2021-03-22 12:11:48 -0400

    functionality correction

--- CH04-Lists/commaCode.py
+++ CH04-Lists/commaCode.py
@@ -2,20 +2,21 @@
 # A function that takes a list and returns a string with all items separated by a comma and a space.
 # Insert "and" before last item and check single and empty list [].
 
-# Example List
-spam = ['apples', 'bananas', 'tofu', 'cats','dog',6,6.5,'humans']
+spam = ['apples', 'bananas', 'tofu', 'cats']
 
+def create_string(list):
+  new_list = ''
+  if len(list) == 0:
+    new_list = 'Please enter a full list'
+  elif len(list) == 1:
+    new_list = "Only {}.".format(list[0])
+  else:
+    for items in list:
+      if list.index(items) != len(list)-1:
+        new_list += "{}, ".format(items)
+      else:
+        new_list += "and {}.".format(items)
+  return new_list
 
-def printList(list):
-    if len(list) == 0:
-        print('Please enter a full list')
-    elif len(list) == 1:
-      print("Only {}.".format(list[0]))
-    else:
-      for items in list:
-        if list.index(items) != len(list)-1:
-          print("{}, ".format(items),end="")
-        else:
-          print("and {}.".format(items),end="")
-
-printList(spam)
+if __name__ == '__main__':
+  print( create_string(spam) )
