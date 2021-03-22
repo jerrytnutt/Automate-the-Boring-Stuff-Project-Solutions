b4e82de280ba44a67fcbfbc336b8d8ee5263e780
Jerry <jerrynutt416@gmail.com>
2021-03-22 12:26:51 -0400

    variable changes

--- CH04-Lists/coinFlipStreaks.py
+++ CH04-Lists/coinFlipStreaks.py
@@ -1,15 +1,16 @@
 # Write a program that Flips a coin 100 times and create a list with the results
-# Find out if a streak of six heads or six tails comes up in the results "HHHHHH" or "TTTTTT"
+# Find out if a streak of six heads or six tails comes up in the results ("HHHHHH" or "TTTTTT")
 # Loop this experiment 10,000 times and return the probability of a streak
 
 import random
 import re
 
-totalRuns = 0            
-streakScore = 0
+total_runs = 0            
+streak_score = 0
 
-def coinFips():
-    global streakScore,totalRuns
+def coin_flips():
+    global streak_score,total_runs
+
     # Return results into a single string for regular expressions
     results = ''
     for i in range(100):
@@ -18,13 +19,13 @@ def coinFips():
         results += "H"
       elif value == 1:
         results += "T"
-    # Use regular expressions to check results sting
     match = re.findall(r'(HHHHHH|TTTTTT)', results)
     if len(match) > 0:
-      streakScore = streakScore +  1
-    totalRuns = totalRuns + 1
+      streak_score = streak_score +  1
+    total_runs = total_runs + 1
+    return total_runs 
 
-for i in range(10000):
-  coinFips()
-
-print("The Chance of a streak is: {}%".format(round(streakScore/totalRuns * 100)))
+if __name__ == '__main__':
+  for i in range(10000):
+    coin_flips()
+  print("The Chance of a streak is: {}%".format(round(streak_score/total_runs * 100)))
