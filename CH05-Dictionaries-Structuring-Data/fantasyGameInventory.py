49365c9dca5522685798e96443088583e6c7dcbb
Jerry <jerrynutt416@gmail.com>
2021-03-22 13:10:05 -0400

    variable changes
    small update

--- CH05-Dictionaries-Structuring-Data/fantasyGameInventory.py
+++ CH05-Dictionaries-Structuring-Data/fantasyGameInventory.py
@@ -1,27 +1,29 @@
 # Fantasy Game Inventory
-# Write a function named displayInventory() that displays any possible “inventory”
-# Write a function named addToInventory(inventory, addedItems)
-# inventory is a dict representing the player’s inventory and addedItems is additional inventory 
-# inventory will receive the new items from addedItems dict 
+# display_inventory that displays any possible “inventory”
+# add_to_Inventory will append addedItems to players inventory
+# The player’s inventory will recieve addedItems and then display the new inventory
 
-playersInventory = {'gold coin': 42, 'rope': 1}
-dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby','ruby']
+players_inventory = {'gold coin': 42, 'rope': 1}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby','ruby']
 
-def displayInventory(dict):
+def display_inventory(dict):
     print("Inventory:")
     item_total = 0
     for keys,values in dict.items():
         print(dict[keys],keys)
         item_total += dict[keys]
-    print("Total number of items: " + str(item_total))
+    return "Total number of inventory items: " + str(item_total)
 
 
-def addToInventory(inventory, addedItems):
+def add_to_Inventory(inventory, addedItems):
     for items in addedItems:
         if items in inventory:
             inventory[items] += 1
         else:
             inventory[items] = 1
-addToInventory(playersInventory,dragonLoot)
-displayInventory(playersInventory)
+    return display_inventory(inventory)
+
+if __name__ == '__main__':
+  print( add_to_Inventory(players_inventory,dragon_loot) )
+  #print( display_inventory(players_inventory) )
 
