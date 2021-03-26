0840f13358e04e660ac14dae48ba367f98f0d094
Jerry <jerrynutt416@gmail.com>
2021-03-26 19:18:05 -0400

    zip file change

--- CH10-Organizing Files/backupToZip.py
+++ CH10-Organizing Files/backupToZip.py
@@ -1,8 +1,9 @@
-# backupToZip.py - Copies an entire folder and its contents into a ZIP file
+# Backing Up a Folder into a ZIP File
+# Copy an entire folder and it's contents into a ZIP file
 # The ZIP file's filename must increment
+
 import os
 import zipfile
-
 
 # Increment number for zip files 
 num = 1
@@ -11,37 +12,35 @@ def backupToZip(path):
   global num
   # While loop to check if a zip file has already been created
   while True:
-    baseName = os.path.basename(path)
-    name = baseName +'_'+str(num)
+    base_name = os.path.basename(path)
+    name = base_name +'_'+str(num)
+
     # Folder path with the zip file and current num
-    zipPath = folderPath+"\\"+name+".zip"
+    zip_path = path+"\\"+name+".zip"
     
     # Num will increment until a zip file with that num does not exist
-    if not os.path.exists(zipPath):
-      
+    if not os.path.exists(zip_path):
       break
     else:
       num = num + 1
   # Create a zip file with current base name and num increment
-  with zipfile.ZipFile(name+'.zip','w') as mzip:
+
+  with zipfile.ZipFile(zip_path,'w') as new_zip:
     # Use os.walk to walk the folder and all subfolders
-    for root,dirs,files in os.walk(path):
-      for folders in dirs:
-      for filenames in files:
-        # Do not zip the previous zip files
-        if filenames.endswith('.zip') and filenames.startswith(baseName):
-          print(filenames.startswith(baseName))
-          
+    for folders, subfolders, filenames in os.walk(path):
+      
+      print("You are now in folder: {}".format(folders))
+      
+      new_zip.write(folders)
+      for files in filenames:
+        print("You are now in file: {}".format(files))
+        
+      # Do not zip the previous zip files
+        if files.endswith('.zip') and files.startswith(base_name):
+          print('skip')
         else:
+          new_zip.write(os.path.join(folders, files))
+    return path
+        
           
-          # Join the files to their root and write
-          mzip.write(os.path.join(root,filenames))
-          
-          
-          
- 
-  
-  
-
-
-backupToZip(FOLDERPATH)
+backupToZip(r"C:\Users\jerry\Documents\chapter9\autor")
